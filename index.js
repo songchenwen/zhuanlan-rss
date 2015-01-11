a82f8eab3718b567d26a683974bd928fee931d90
songchenwen <emptyzone.0@gmail.com>
2015-01-11 21:03:36 +0800

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+var ids = ['yeka52', 'maboyong', 'datouma', 'gaizhilizcw', 'tianhao', 'qinnan', 'lianghuan', 'talich', 
+'loveletter', 'zenithdie', 'xiaolei', 'Glasschurch', 'nosensedigit', 'oldplusnew', 'negative2', 
+'taosay', 'DKLearnsPop', 'mactalk', 'lswlsw', 'rosicky311', 'zhimovie', 'liangbianyao', 
+'bianzhongqingnianxingdongzhinan', 'phos-study', 'gushidawang-Lipen'];
+
+var rssPath = 'out/zhuanlanrss.xml';
+
+require('./lib/rss')(ids, rssPath);
+
