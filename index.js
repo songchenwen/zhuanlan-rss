3c44bf34f1fee345da546ddba5362f195895b813
songchenwen <sanvinsung@gmail.com>
2015-08-26 16:18:40 +0800

    Add Simple Cache

--- index.js
+++ index.js
@@ -4,19 +4,37 @@ var ids = ['yeka52', 'maboyong', 'datouma', 'gaizhilizcw', 'tianhao', 'qinnan', 
 'bianzhongqingnianxingdongzhinan', 'phos-study', 'wontfallinyourlap', '24frames', 'wuliang8910'];
 
 var rss = require('./lib/rss');
+var fs = require('fs');
+var express = require('express');
+var app = express();
 
 var rssPath = __dirname + '/out/zhuanlanrss.xml';
 
-var express = require('express');
-var app = express();
+
+var lastUpdate = 0;
+var cacheTime = 15 * 60 * 1000;
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
+	if(fs.existsSync(rssPath)){
+		if(new Date().getTime() - lastUpdate < cacheTime){
+			console.log('response from cache');
+			response.status(200).sendFile(rssPath);
+			return;
+		}else{
+			console.log('cache expired');
+		}
+	}else{
+		console.log('no cache file found');
+	}
+	console.log('begin request from zhihu');
   rss(ids, rssPath, function(err){
   	if(err){
+  		lastUpdate = 0;
   		response.status(500).send(err);
   	}else{
+  		lastUpdate = new Date().getTime();
   		response.status(200).sendFile(rssPath);
   	}
   });
@@ -24,5 +42,4 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-  rss(ids, rssPath);
 });
