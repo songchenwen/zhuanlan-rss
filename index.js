a13121aec3e3e769f54f3fe6e606839c33723baf
songchenwen <sanvinsung@gmail.com>
2015-08-26 15:44:49 +0800

    Move to Heroku

--- index.js
+++ index.js
@@ -3,7 +3,26 @@ var ids = ['yeka52', 'maboyong', 'datouma', 'gaizhilizcw', 'tianhao', 'qinnan', 
 'taosay', 'DKLearnsPop', 'mactalk', 'lswlsw', 'rosicky311', 'zhimovie', 'liangbianyao', 
 'bianzhongqingnianxingdongzhinan', 'phos-study', 'wontfallinyourlap', '24frames', 'wuliang8910'];
 
-var rssPath = 'out/zhuanlanrss.xml';
+var rss = require('./lib/rss');
 
-require('./lib/rss')(ids, rssPath);
+var rssPath = __dirname + '/out/zhuanlanrss.xml';
 
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+  rss(ids, rssPath, function(err){
+  	if(err){
+  		response.status(500).send(err);
+  	}else{
+  		response.status(200).sendFile(rssPath);
+  	}
+  });
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+  rss(ids, rssPath);
+});
