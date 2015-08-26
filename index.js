d8c46856e8f7dfa5877ac162f93de28c46eac0f2
songchenwen <sanvinsung@gmail.com>
2015-08-26 10:07:09 +0800

    Add One

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var ids = ['yeka52', 'maboyong', 'datouma', 'gaizhilizcw', 'tianhao', 'qinnan', 'lianghuan', 'talich', 
 'loveletter', 'zenithdie', 'Glasschurch', 'nosensedigit', 'oldplusnew', 'negative2', 
 'taosay', 'DKLearnsPop', 'mactalk', 'lswlsw', 'rosicky311', 'zhimovie', 'liangbianyao', 
-'bianzhongqingnianxingdongzhinan', 'phos-study', 'wontfallinyourlap', '24frames'];
+'bianzhongqingnianxingdongzhinan', 'phos-study', 'wontfallinyourlap', '24frames', 'wuliang8910'];
 
 var rssPath = 'out/zhuanlanrss.xml';
 
