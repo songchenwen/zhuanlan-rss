53634022f6194ad2af61659184845e96b7314efd
songchenwen <me@songchenwen.com>
2015-09-07 21:12:48 +0800

    Use Redis

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-var ids = ['yeka52', 'maboyong', 'datouma', 'gaizhilizcw', 'tianhao', 'qinnan', 'lianghuan', 'talich', 
-'loveletter', 'zenithdie', 'Glasschurch', 'nosensedigit', 'oldplusnew', 'negative2', 
-'taosay', 'DKLearnsPop', 'mactalk', 'lswlsw', 'rosicky311', 'zhimovie', 'liangbianyao', 
-'bianzhongqingnianxingdongzhinan', 'phos-study', 'wontfallinyourlap', '24frames', 'wuliang8910'];
 
-var rss = require('./lib/rss');
-var fs = require('fs');
 var express = require('express');
+var storage = require('./lib/storage');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 var xmlHeader = '<?xml version="1.0" encoding="UTF-8"?>';
 
-app.get('/', function(request, response) {
-
+app.get('/rss/:ids', function(request, response) {
+	var ids = request.params.ids.split(',');
+	var rss = require('./lib/rss');
+	rss.storage = storage;
+	console.log('request ids ' + ids.join(','));
 	response.set('Content-Type', 'text/xml');
 	response.status('200').write(xmlHeader + '\n');
 
 	var keepAliveTimer = setInterval(function(){
 		console.log('keep alive heart beat');
-		response.write('<!-- fetching -->\n');
-	}, 20000);
+		response.write(' \n');
+	}, 10000);
 
-	rss(ids, function(err, xml){
+	rss.get(ids, function(err, xml){
 		clearInterval(keepAliveTimer);
 		if(err){
 			response.write(err);
